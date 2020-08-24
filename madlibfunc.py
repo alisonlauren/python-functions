24f8ca59477bbbe2b95c880c26fa76f99708594f
Alison Manning <alisonlaurenmanning@gmail.com>
2020-08-24 16:08:44 -0500

    adding file

---
+++ madlibfunc.py
@@ -0,0 +1,14 @@
+def madlib(name, subject):
+    if True:
+        return "My name is", name, "My favorite subject is", subject
+        
+
+while True:
+    name = input("Pick a name: ")
+    subject = input("Pick a subject: ")
+    message = madlib(name, subject)
+    print(message)
+
+    
+        
+
