81e9e9abae3ffe2b2db49b1357dccdd9e0b7e5c8
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-14 13:45:55 -0600

    simplified file

--- dictionary1.py
+++ dictionary1.py
@@ -7,12 +7,17 @@ phonebook_dict = {
 
 print(" ")
 print("======Small Exercise One=========")
+#Print Elizabeth's phone number.
 print("Elizabeth's phone number ")
 print(phonebook_dict["Elizabeth"])
+#Add an entry of kareem and his number
 phonebook_dict['Kareem'] = '938-489-1234'
+# Delete Alice's phone entry.
 phonebook_dict.pop('Alice')
+# Change Bob's phone number to '968-345-2345'.
 phonebook_dict['Bob'] = '968-345-2345'
 print("Final Product: ")
+# Print all phone entries now that they are editied
 print(phonebook_dict)
 print("==================================")
 print(" ")
@@ -39,12 +44,16 @@ ramit = {
 print(" ")
 print("======Small Exercise Two=========")
 print("  1. Ramit's email address:  ")
+# Write a python expression that gets the email address of Ramit.
 print(ramit['email'])
 print("  2. First of Ramit's interests:  ")
+# Write a python expression that gets the first of Ramit's interests.
 print(ramit['interests'][0])
 print("  3. Jasmine's email address:  ")
+#Write a python expression that gets the email address of Jasmine.
 print(ramit['friends'][0]['email'])
 print("  4. Second of Jan's interests:           ")
+# Write a python expression that gets the second of Jan's two interests.
 print(ramit['friends'][1]['interests'][1])
 print("==================================")
 print(" ")
