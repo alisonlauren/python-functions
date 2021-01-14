d82bc74099ace28e9412b9ff76faedff6fcee7ee
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-14 14:45:33 -0600

    adding how to use import pickle

--- dictionary1.py
+++ dictionary1.py
@@ -90,3 +90,22 @@ print(countFriends(ramit))
 print(" ")
 
 
+
+
+
+#importing data into a file.
+
+##import pickle
+
+#with open('heroes.txt', 'rb') as file_handle
+    # old_message = pickle.load(file_handle)
+    # print(f'Old message is: "{old_message}"')
+
+## message = "marvel is better than dc"
+
+
+#with open('heroes.txt', 'wb') as file_handle:
+    #pickle.dump(user_input, file_handle)
+
+
+
