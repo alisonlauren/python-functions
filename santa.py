9c3775899f183fa41aa31586cfcb07efbd45f141
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-18 07:33:59 -0600

    santa question answered

--- santa.py
+++ santa.py
@@ -1,3 +1,13 @@
+
+##question 1:
+gift_request = input("Hello, what would you like for christmas? ")
 user_input = input("Santa: Have you been good or bad this year?  ")
-def beSanta(gift):
-    if user_input.lower() == "good"
+
+if user_input.lower() == 'good':
+    print(f"Okay, good for you, you get the {gift_request}")
+elif user_input.lower() == 'bad':
+    print(f"I don't feel bad for you, you'll never get {gift_request} ")
+else:
+    print("Try again, either say good or bad")
+
+#question 2:
